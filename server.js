e2775224fc52b723c21636c068e8703b362af672
houslast3 <houslast3@gmail.com>
2024-11-27 15:28:22 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -305,6 +305,10 @@ app.post('/api/tables/:id/close', async (req, res) => {
 });
 
 // Rotas das interfaces
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/waiter', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'waiter.html'));
 });
